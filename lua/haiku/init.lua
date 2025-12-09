0db033b68fae6e9ddb10ed254ac663ca2f420eeb
Andor <juuso.melentjeff@gmail.com>
2025-12-09 20:19:41 +0200

    refactor: correct indenting after fixing autoformatter config

--- lua/haiku/init.lua
+++ lua/haiku/init.lua
@@ -6,29 +6,29 @@ M.haikus_panel = nil
 M.setup = function(opts)
   opts = opts or {}
 
-  M.haikus_path = opts.haikus_path or vim.fn.expand("~/.local/share/nvim/haiku/haikus.md")
+  M.haikus_path = opts.haikus_path or vim.fn.expand '~/.local/share/nvim/haiku/haikus.md'
   M.create_haikus_file()
 
   M.daily_headers = opts.daily_headers or false
 
-  M.keymaps = vim.tbl_deep_extend("force", {
-    toggle_add_haiku = "<Leader>h",
-    toggle_haikus = "<Leader>H",
+  M.keymaps = vim.tbl_deep_extend('force', {
+    toggle_add_haiku = '<Leader>h',
+    toggle_haikus = '<Leader>H',
   }, opts.keymaps or {})
 
-  vim.api.nvim_create_user_command("Haiku", function()
+  vim.api.nvim_create_user_command('Haiku', function()
     M.toggle_add_haiku()
   end, {})
 
   vim.api.nvim_set_keymap(
-    "n",
+    'n',
     M.keymaps.toggle_add_haiku,
     ':lua require("haiku").toggle_add_haiku()<CR>',
     { noremap = true, silent = true }
   )
 
   vim.api.nvim_set_keymap(
-    "n",
+    'n',
     M.keymaps.toggle_haikus,
     ':lua require("haiku").toggle_haikus()<CR>',
     { noremap = true, silent = true }
@@ -36,17 +36,17 @@ M.setup = function(opts)
 end
 
 M.get_date_header = function()
-  local todays_date = os.date("%d-%m-%Y")
-  return "## " .. todays_date
+  local todays_date = os.date '%d-%m-%Y'
+  return '## ' .. todays_date
 end
 
 M.create_haikus_file = function()
   if vim.fn.filereadable(M.haikus_path) ~= 1 then
-    local dir_path = vim.fn.fnamemodify(M.haikus_path, ":h")
-    vim.fn.mkdir(dir_path, "p")
-    local file = io.open(M.haikus_path, "w")
+    local dir_path = vim.fn.fnamemodify(M.haikus_path, ':h')
+    vim.fn.mkdir(dir_path, 'p')
+    local file = io.open(M.haikus_path, 'w')
     if file then
-      file:write("# Haikus\n\n")
+      file:write '# Haikus\n\n'
       file:close()
     end
   end
@@ -69,7 +69,7 @@ M.get_insertion_point = function(lines, header_line)
 
   for i = header_line + 1, #lines do
     local line = lines[i]
-    if line:match("^##%s") then
+    if line:match '^##%s' then
       return i
     end
   end
@@ -78,12 +78,12 @@ M.get_insertion_point = function(lines, header_line)
 end
 
 M.setup_buffer_options = function(bufnr)
-  vim.api.nvim_buf_set_option(bufnr, "filetype", "markdown")
-  vim.api.nvim_buf_set_option(bufnr, "buftype", "nofile")
-  vim.api.nvim_buf_set_option(bufnr, "bufhidden", "wipe")
-  vim.api.nvim_buf_set_option(bufnr, "modified", false)
+  vim.api.nvim_buf_set_option(bufnr, 'filetype', 'markdown')
+  vim.api.nvim_buf_set_option(bufnr, 'buftype', 'nofile')
+  vim.api.nvim_buf_set_option(bufnr, 'bufhidden', 'wipe')
+  vim.api.nvim_buf_set_option(bufnr, 'modified', false)
 
-  vim.api.nvim_create_autocmd("BufWinLeave", {
+  vim.api.nvim_create_autocmd('BufWinLeave', {
     buffer = bufnr,
     callback = function()
       if M.haikus_winnr then
@@ -92,7 +92,7 @@ M.setup_buffer_options = function(bufnr)
     end,
   })
 
-  vim.api.nvim_create_autocmd("QuitPre", {
+  vim.api.nvim_create_autocmd('QuitPre', {
     buffer = bufnr,
     callback = function()
       M.save_and_close()
@@ -108,7 +108,7 @@ M.save_and_close = function()
 
     local has_content = false
     for _, line in ipairs(lines) do
-      if line:match("%S") then
+      if line:match '%S' then
         has_content = true
         break
       end
@@ -125,7 +125,7 @@ M.save_and_close = function()
         local current_lines = vim.api.nvim_buf_get_lines(haikus_bufnr, 0, -1, false)
 
         local new_content = {}
-        table.insert(new_content, "")
+        table.insert(new_content, '')
 
         for _, line in ipairs(lines) do
           table.insert(new_content, line)
@@ -146,13 +146,13 @@ M.save_and_close = function()
         end
 
         vim.api.nvim_buf_call(haikus_bufnr, function()
-          vim.cmd("silent write")
+          vim.cmd 'silent write'
         end)
 
-        vim.notify("Haiku saved", vim.log.levels.INFO)
+        vim.notify('Haiku saved', vim.log.levels.INFO)
       end
     end
-    vim.api.nvim_buf_set_option(bufnr, "modified", false)
+    vim.api.nvim_buf_set_option(bufnr, 'modified', false)
     vim.api.nvim_win_close(M.haikus_winnr, true)
     M.haikus_winnr = nil
   end
@@ -161,10 +161,10 @@ end
 M.discard_and_close = function()
   if M.haikus_winnr and vim.api.nvim_win_is_valid(M.haikus_winnr) then
     local bufnr = vim.api.nvim_win_get_buf(M.haikus_winnr)
-    vim.api.nvim_buf_set_option(bufnr, "modified", false)
+    vim.api.nvim_buf_set_option(bufnr, 'modified', false)
     vim.api.nvim_win_close(M.haikus_winnr, true)
     M.haikus_winnr = nil
-    vim.notify("Note discarded", vim.log.levels.INFO, { title = "Haiku" })
+    vim.notify('Note discarded', vim.log.levels.INFO, { title = 'Haiku' })
   end
 end
 
@@ -176,47 +176,47 @@ M.create_floating_window = function()
   local row = math.floor((vim.o.lines - height) / 2)
 
   local opts = {
-    relative = "editor",
+    relative = 'editor',
     width = width,
     height = height,
     col = col,
     row = row,
-    style = "minimal",
-    border = "rounded",
-    title = "Haiku",
-    title_pos = "center",
+    style = 'minimal',
+    border = 'rounded',
+    title = 'Haiku',
+    title_pos = 'center',
   }
 
   local buffer = vim.api.nvim_create_buf(false, true)
   M.setup_buffer_options(buffer)
   vim.api.nvim_buf_set_keymap(
     buffer,
-    "n",
-    "<CR>",
+    'n',
+    '<CR>',
     '<cmd>lua require("haiku").save_and_close()<CR>',
-    { noremap = true, desc = "Save note and close window" }
+    { noremap = true, desc = 'Save note and close window' }
   )
 
   vim.api.nvim_buf_set_keymap(
     buffer,
-    "n",
-    "<Esc>",
+    'n',
+    '<Esc>',
     '<cmd>lua require("haiku").save_and_close()<CR>',
-    { noremap = true, silent = true, desc = "Save note and close" }
+    { noremap = true, silent = true, desc = 'Save note and close' }
   )
 
   vim.api.nvim_buf_set_keymap(
     buffer,
-    "i",
-    "<C-c>",
+    'i',
+    '<C-c>',
     '<cmd>lua require("haiku").discard_and_close()<CR>',
-    { noremap = true, silent = true, desc = "Discard note and close" }
+    { noremap = true, silent = true, desc = 'Discard note and close' }
   )
 
   local winnr = vim.api.nvim_open_win(buffer, true, opts)
-  vim.cmd("startinsert")
-  vim.api.nvim_win_set_option(winnr, "winblend", 10)
-  vim.api.nvim_win_set_option(winnr, "cursorline", true)
+  vim.cmd 'startinsert'
+  vim.api.nvim_win_set_option(winnr, 'winblend', 10)
+  vim.api.nvim_win_set_option(winnr, 'cursorline', true)
 
   return winnr
 end
@@ -239,27 +239,27 @@ M.create_floating_panel = function()
   local row = 0
 
   local opts = {
-    relative = "win",
+    relative = 'win',
     win = 0,
     width = width,
     height = win_height,
     col = col,
     row = row,
-    anchor = "NW",
-    style = "minimal",
+    anchor = 'NW',
+    style = 'minimal',
   }
 
   local bufnr = M.get_haikus_buffer()
 
-  vim.api.nvim_buf_set_option(bufnr, "filetype", "markdown")
-  vim.api.nvim_buf_set_option(bufnr, "bufhidden", "hide")
-  vim.api.nvim_buf_set_option(bufnr, "buftype", "")
-  vim.api.nvim_buf_set_option(bufnr, "swapfile", false)
-  vim.api.nvim_buf_set_option(bufnr, "modified", false)
+  vim.api.nvim_buf_set_option(bufnr, 'filetype', 'markdown')
+  vim.api.nvim_buf_set_option(bufnr, 'bufhidden', 'hide')
+  vim.api.nvim_buf_set_option(bufnr, 'buftype', '')
+  vim.api.nvim_buf_set_option(bufnr, 'swapfile', false)
+  vim.api.nvim_buf_set_option(bufnr, 'modified', false)
 
   local winnr = vim.api.nvim_open_win(bufnr, true, opts)
 
-  vim.api.nvim_win_set_option(winnr, "winhl", "Normal:PanelNormal")
+  vim.api.nvim_win_set_option(winnr, 'winhl', 'Normal:PanelNormal')
 
   return winnr
 end
